3c91e1b211e68f80e1f56a1306e2078008999877
restart0x <161155505+restart0x@users.noreply.github.com>
2024-09-12 10:24:50 -0400

    Developed index.js

---
+++ index.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const Discord = require('discord.js');
+require('dotenv').config(); 
+
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+}
+
+const TOKEN = process.env.DISCORD_BOT_TOKEN;
+
+client.once('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', message => {
+    if (!message.content.startsWith(process.env.PREFIX) || message.author.bot) return;
+
+    const args = message.content.slice(process.env.PREFIX.length).trim().split(/ +/);
+    const commandName = args.shift().toLowerCase();
+
+    const command = client.commands.get(commandName)
+        || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+
+    if (!command) return; 
+
+    try {
+        command.execute(message, args);
+    } catch (error) {
+        console.error(error);
+        message.reply('there was an error trying to execute that command!');
+    }
+});
+
+client.login(TOKEN);
